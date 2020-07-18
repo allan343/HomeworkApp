e994a180ca7cab42f69ad0871e398d7189bd5e18
Allan Cheng <allanhfcheng@gmail.com>
2020-07-17 23:01:30 -0700

    Calender Page state

--- my-homework-app/src/Calender/Calender.js
+++ my-homework-app/src/Calender/Calender.js
@@ -8,16 +8,41 @@ import HeaderNav from '../HeaderNav/HeaderNav';
 export default class Calender extends React.Component {
 
   static contextType = ApiContext;
+/*
+  static defaultProps = {
+      day: ["Mon","Tue","Wed","Thurs","Fri","Sat","Sun"],
+      dayOfWeek:''
+  };
+*/
+  constructor(props) {
+    //states a show can have
+    super(props);
+    this.state = {
+      day: ["Mon","Tue","Wed","Thurs","Fri","Sat","Sun"],
+      dayOfWeek: ''
+    };
 
-   //filter shows by their not yet watching state
-  //only show shows that have that state for this view
+   // this.updateClass = this.updateClass.bind(this);
+  }
+
+  updateDayOfWeek(date) {
+    console.log(date);
+    var selectedDate = new Date(date);
+    var dayOfWeekIndex = selectedDate.getDay();
+    console.log(dayOfWeekIndex);
+    console.log(this.state.day[dayOfWeekIndex] );
+    this.setState({ dayOfWeek: this.state.day[dayOfWeekIndex] });
+  }
+
   render() {
+
+    
     return (
       <span className='NoteListMain__button-container'>
        
        <HeaderNav></HeaderNav>
        <label for="startDate">Select a day</label>
-            <input type="date" id="startDate" name="startDate" onChange={e => this.updateStartDate(e.target.value)} ></input>
+            <input type="date" id="startDate" name="startDate" onChange={e => this.updateDayOfWeek(e.target.value)} ></input>
         Classes:
        <SchoolClassList schoolClasses={this.context.schoolClasses} ></SchoolClassList>
        Homework due that day:
